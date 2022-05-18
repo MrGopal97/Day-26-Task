18db288ffb16a484cff04df1d3cdcf4f3a6e5249
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-05-18 11:40:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./Price.css";
+import PriceCard from "./priceCard";
+import priceData from "./PricingData";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { priceData };
+  }
+  render() {
+    // console.log(this.state);
+    return (
+      <section className="pricing py-5">
+        <div className="container">
+          <div className="row">
+            {this.state.priceData.map(
+              ({ title, price, pricePer, currency, features, strong }) => {
+                return (
+                  <PriceCard
+                    title={title}
+                    price={price}
+                    pricePer={pricePer}
+                    currency={currency}
+                    features={features}
+                    strong={strong}
+                    key={price}
+                  />
+                );
+              }
+            )}
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
 
 export default App;
